cbab108456ff990459befee371a3e085d031f1ff
Wai Yan Myint <myint.waiyan1996@gmail.com>
2020-09-13 20:51:59 +0630

    All File

---
+++ ExtractPhotofromVideo.py
@@ -0,0 +1,20 @@
+import cv2
+import os
+
+cap = cv2.VideoCapture("wyh.mp4")
+index = 1
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    # defaultpath = "/wyh/"
+    # filename = str(index) + ".jpg"
+    # file = os.path.join(defaultpath,filename)
+    # print(file)
+    cv2.imwrite(str(index)+'.jpg',frame)
+
+    if ret == False or index == 60:
+        break
+    index += 1
+
+cap.release()
+cv2.destroyAllWindows()
